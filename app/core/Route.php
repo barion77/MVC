2a97f902a827118d22156998161f9e8d67423011
barion77 <barion224@gmail.com>
2022-08-02 13:00:52 +0300

    route update

--- app/core/Route.php
+++ app/core/Route.php
@@ -49,11 +49,128 @@ class Route
         $route = ['uri', 'params', 'method'];
 
         if ($method == 'POST' && $current_uri == $uri) {
-            $route = array_combine($route, array($current_uri, $params, $method));
             $controller = 'app\controllers\\' . explode('@', $params)[0];
             $action = explode('@', $params)[1];
             if (class_exists($controller)) {
                 if (method_exists($controller, $action)) {
+                    $route = array_combine($route, array($current_uri, $params, $method));
+                    $controller = new $controller($route);
+                    $controller->$action();
+                } else {
+                    if (Config::debug) {
+                        throw new Exception(`Action: $action does not exist in $controller`);
+                    } else {
+                        abort(404, 'Not Found');
+                    }
+                }
+            } else {
+                if (Config::debug) {
+                    throw new Exception(`Class: $controller does not exist`);
+                } else {
+                    abort(404, 'Not Found');
+                }
+            }
+        }
+    }
+
+    public static function put($uri, $params)
+    {
+        self::$routes[] = $uri;
+
+        $method = '';
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['_method'])) {
+                $method = strtoupper($_POST['_method']);  
+            }
+        }
+
+        $current_uri = $_SERVER['REQUEST_URI'];
+        $route = ['uri', 'params', 'method'];
+
+        if ($method == 'PUT' && $current_uri == $uri) {
+            $controller = 'app\controllers\\' . explode('@', $params)[0];
+            $action = explode('@', $params)[1];
+            if (class_exists($controller)) {
+                if (method_exists($controller, $action)) {
+                    $route = array_combine($route, array($current_uri, $params, $method));
+                    $controller = new $controller($route);
+                    $controller->$action();
+                } else {
+                    if (Config::debug) {
+                        throw new Exception(`Action: $action does not exist in $controller`);
+                    } else {
+                        abort(404, 'Not Found');
+                    }
+                }
+            } else {
+                if (Config::debug) {
+                    throw new Exception(`Class: $controller does not exist`);
+                } else {
+                    abort(404, 'Not Found');
+                }
+            }
+        }
+    }
+
+    public static function patch($uri, $params)
+    {
+        self::$routes[] = $uri;
+
+        $method = '';
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['_method'])) {
+                $method = $_POST['_method'];
+            }
+        }
+
+        $current_uri = $_SERVER['REQUEST_URI'];
+        $route = ['uri', 'params', 'method'];
+
+        if ($method == 'PATCH' && $current_uri == $uri) {
+            $controller = 'app\controllers\\' . explode('@', $params)[0];
+            $action = explode('@', $params)[1];
+            if (class_exists($controller)) {
+                if (method_exists($controller, $action)) {
+                    $route = array_combine($route, array($current_uri, $params, $method));
+                    $controller = new $controller($route);
+                    $controller->$action();
+                } else {
+                    if (Config::debug) {
+                        throw new Exception(`Action: $action does not exist in $controller`);
+                    } else {
+                        abort(404, 'Not Found');
+                    }
+                }
+            } else {
+                if (Config::debug) {
+                    throw new Exception(`Class: $controller does not exist`);
+                } else {
+                    abort(404, 'Not Found');
+                }
+            }
+        }
+    }
+
+    public static function delete($uri, $params)
+    {
+        self::$routes[] = $uri;
+
+        $method = '';
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (isset($_POST['_method'])) {
+                $method = $_POST['_method'];
+            }
+        }
+
+        $current_uri = $_SERVER['REQUEST_URI'];
+        $route = ['uri', 'params', 'method'];
+
+        if ($method == 'DELETE' && $current_uri == $uri) {
+            $controller = 'app\controllers\\' . explode('@', $params)[0];
+            $action = explode('@', $params)[1];
+            if (class_exists($controller)) {
+                if (method_exists($controller, $action)) {
+                    $route = array_combine($route, array($current_uri, $params, $method));
                     $controller = new $controller($route);
                     $controller->$action();
                 } else {
